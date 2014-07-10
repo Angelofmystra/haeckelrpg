281cc47188c75284aef61449a334d21c49f09587
Bertrand Brompton <bertrandbrompton@gmail.com>
2014-07-10 14:45:44 +0100

    Polls code quality improved to PEP8 standard.

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-from django.template import RequestContext, loader
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from polls.models import Choice, Poll
+
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
     context = {'latest_poll_list': latest_poll_list}
     return render(request, 'polls/index.html', context)
 
-#def index(request):
+# def index(request):
 #    return HttpResponse("Hello, world. You're at the poll index.")
 
 # Create your views here.
+
+
 def detail(request, poll_id):
     poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
 
+
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
